233a5603a566e74398839be8ca1a46986cf790f6
CobbleWorksMC <CobbleWorksMC@users.noreply.github.com>
2017-08-15 16:10:17 -0500

    Delete TileEntityRefinery.java

--- main/java/curtis/Cobbleworks/module/refinery/TileEntityRefinery.java
+++
@@ -1,8 +0,0 @@
-package curtis.Cobbleworks.module.refinery;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityRefinery extends TileEntity {
-	
-	public TileEntityRefinery() {}
-}
